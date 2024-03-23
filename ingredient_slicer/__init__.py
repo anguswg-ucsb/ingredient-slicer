2d9b9a24046b6a757915f0689751004ecf49d409
anguswg-ucsb <anguswatters@gmail.com>
2024-03-23 15:51:17 -0700

    bumped version, and published to pypi

--- ingredient_slicer/__init__.py
+++ ingredient_slicer/__init__.py
@@ -1,6 +1,6 @@
 # __init__.py
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 from ._constants import NUMBER_WORDS,  NUMBER_PREFIX_WORDS, \
     MULTI_FRACTION_WORDS, FRACTION_WORDS, DENOMINATOR_WORDS, UNICODE_FRACTIONS, \
